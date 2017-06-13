eb996b03b82514712a5f951cf989937dd11c14fd
Vitaliy Vorynka <vorynkavetal@gmail.com>
2017-06-13 15:04:10 +0300

    8

--- stenserBrother/js/script.js
+++ stenserBrother/js/script.js
@@ -1,4 +1,4 @@
-var dh = $(document).height();
+var dh = $(window).height();
 $('.page-head').css('height', dh);
 
 $('.toggle').click(function(){
@@ -9,7 +9,19 @@ $('.toggle').click(function(){
     	top: '65px'
 	});
 	// $('.head').fadeOut();
-	$('em').toggleClass('close');
+	$('.click').toggleClass('close');
 })
+$(".menu-side").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+$('.arrow-down').click(function () {
+	var elementClick = $('#about')
+	var destination = $(elementClick).offset().top;
+	jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1500);
+		return false;
+});
 
 
