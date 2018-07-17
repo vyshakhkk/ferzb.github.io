8bb5d6d3521e4513f16b65e926d7f2fb29763751
ferzb <vorynkavetal@gmail.com>
2018-07-17 11:06:14 +0300

    asd
    ad

--- nomidman/js/script.js
+++ nomidman/js/script.js
@@ -9,3 +9,18 @@ $(".js_nav-item").on("click","a", function (event) {
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+$('body').append('<i class="btn-up">');
+
+$('.btn-up').click(function(){
+	$('body').animate({'scrollTop': 0}, 1000);
+	$('html').animate({'scrollTop': 0}, 1000)
+})
+$(window).scroll(function(){
+	if ($(window).scrollTop() > 200){
+		$('.btn-up').addClass('active');
+	}
+	else{
+		$('.btn-up').removeClass('active');
+	}
+});
