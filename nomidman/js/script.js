b62be092280c8896fb6a21f64ac72378744cfcb3
ferzb <vorynkavetal@gmail.com>
2018-07-20 10:03:50 +0300

    asdfasdf
    asdasd

--- nomidman/js/script.js
+++ nomidman/js/script.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+	setTimeout(function() {
+		var preloader = $('#page-preloader');
+		if ( !preloader.hasClass('done')) {
+			preloader.addClass('done')
+		}
+	}, 2000)
+});
 $('#nav-toggle').click(function(){
 	$('.toggle-navbar').toggleClass('open-nav')
 	$('.nav-toggle').toggleClass('open-toggle')
@@ -23,4 +31,24 @@ $(window).scroll(function(){
 	else{
 		$('.btn-up').removeClass('active');
 	}
+});
+$('.faq-list-item').click(function(){
+	var id  = $(this).attr('name')
+	$('.faq-list-item').removeClass('active-item')
+	$(this).addClass('active-item')
+	$('.general-quetion').removeAttr('style')
+	$('div[id=' +id+ ']').fadeIn()	
+})
+
+$('.open-faq-list').click(function() {
+	$('.faq-list-item').css({
+		display: 'block'
+	});;
+	$('.faq-list-item').click(function(){
+		$('.faq-list-item').removeAttr('style')
+		$(this).addClass('active-item')
+	})
+});
+$('.question-item').click(function() {
+	$(this).children('.inner').slideToggle()
 });
