d34479bcdcb6372c0937734734ae3e32c8b1515c
ferzb <vorynkavetal@gmail.com>
2018-10-18 12:14:15 +0300

    asdg
    asgda

--- beyoud/js/script.js
+++ beyoud/js/script.js
@@ -4,3 +4,59 @@ $('.nav-toggle-block').click(function() {
 	$('nav').toggleClass('nav-open');
 	$('header').toggleClass('header-menu-open');
 });
+
+$(function() {
+	$(window).scroll(function(){
+		if  ($(window).scrollTop() > 100)
+			$('header').addClass('header-fixed');
+
+		else
+			$('header').removeClass('header-fixed');
+
+	});
+});
+var time = 2, show = 1 ;
+	$(window).scroll(function() {
+		$('#counter').each(function(){
+			var
+				cPos = $(this).offset().top,
+				topWindow = $(window).scrollTop();
+			if(cPos < topWindow + 500){
+				if(show < 2){
+					$('.circul').addClass('viz');
+					$('p').each(function(){
+					var 
+						i = 1,
+						num = $(this).data('num'),
+						step = 1000 * time / num,
+						that = $(this),
+						int = setInterval(function(){
+							if (i <= num) {
+								that.html(i);
+							}
+							else {
+								show = show + 2
+								clearInterval(int);
+							}
+							i++;
+						},step);
+					});
+				}	
+			}
+		});
+});
+
+$('.owl-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:true,
+    nav:true,
+    margin:10
+});
+
+$('.user-slider').owlCarousel({
+	items:2,
+    loop:false,
+    margin:10,
+    nav:false,
+})
