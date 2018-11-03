fec30417e46e4c075efe4730b0647537dc56f9b6
ferzb <vorynkavetal@gmail.com>
2018-11-03 11:42:10 +0200

    sdfhsdf
    hsdhs

--- mockup/js/script.js
+++ mockup/js/script.js
@@ -2,4 +2,14 @@ function date() {
 	var valStart = $('#dateStart').val();
 	var valFinal = $('#dateFinal').val();
 	$('.p-date-time').text(''+valStart+' - ' +valFinal);
+
 }
+
+$('.tab-chart').click(function() {
+	var id =$(this).attr('name')
+	$('.tab-chart').removeClass('active-tab')
+	$(this).addClass('active-tab')
+	$('.tab-chart-block').removeAttr('style')
+	$('div[id=' +id+ ']').fadeIn().css({
+		display: 'block'
+	});});
