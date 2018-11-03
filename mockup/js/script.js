892e3307bf61fc707ba878c8f48675f33af0ce50
ferzb <vorynkavetal@gmail.com>
2018-11-03 15:13:13 +0200

    asdgasdg
    adasgasd

--- mockup/js/script.js
+++ mockup/js/script.js
@@ -1,7 +1,13 @@
 $(function() {
   $('input[name="daterange"]').daterangepicker({
-    opens: 'right'
+    opens: 'bottom'
+  }, function(start, end, label) {
+  	$('.p-date-time').text(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
   });
+});
+
+$('.search-btn').click(function() {
+	$(this).toggleClass('search-open');
 });
 
 $('.tab-chart').click(function() {
