6f4e6fdfcedfc8bb5c8d24960943379b8f8b4cd1
ferzb <vorynkavetal@gmail.com>
2018-11-03 09:51:37 +0200

    sdgsdg
    fsgdfg

--- mockup/js/script.js
+++ mockup/js/script.js
@@ -1,7 +1,5 @@
-$('.nav-toggle-block').click(function() {
-	$(this).toggleClass('open');
-	$('.nav-toggle-line').toggleClass('active-line');
-	$('.nav-list>li').toggleClass('active-list');
-	$('.wrap-nav').toggleClass('wrap-nav-open');
-	$('.nav-page').toggleClass('background-black');
-});
+function date() {
+	var valStart = $('#dateStart').val();
+	var valFinal = $('#dateFinal').val();
+	$('.p-date-time').text(''+valStart+' - ' +valFinal);
+}
