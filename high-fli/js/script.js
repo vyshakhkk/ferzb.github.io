ce34e9af331f72979a09140132507b1d0468b988
ferzb <vorynkavetal@gmail.com>
2018-12-31 21:36:54 +0200

    gsdfsg
    dfgsdf

--- high-fli/js/script.js
+++ high-fli/js/script.js
@@ -3,28 +3,22 @@ $(document).ready(function() {
 		links = menuList.find('> a'),
 		btn = $('.global-btn');
 
-	$('.nav-toggle-block').click(function() {
-		$('.nav-toggle-line').toggleClass('active-line');
-		$('header').toggleClass('page-fixed-header-open');
-		$('.logo').toggleClass('logo-open');
-		$('nav').slideToggle();
-	});
+	var toggleIcon = $('#show');
+	var closeToggle = $('.close');
+	var nav = $('#nav');
 
-	$(window).resize(function() {
-		if ($(window).width() > 780) {
-			$('nav').removeAttr('style')
-			$('.page-header').removeClass('page-fixed-header-open');
-		}
+	toggleIcon.on('click', function(event) {
+		nav.addClass('active')
 	});
-
+	closeToggle.on('click', function(event) {
+		nav.removeClass('active')
+	});
 	links.on("click", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1000);
-        $('header').toggleClass('page-fixed-header-open');
-        $('.nav-toggle-line').toggleClass('active-line');
-        $('nav').slideToggle();
+    	nav.removeClass('active');
     });
     btn.on("click", function (event) {
         event.preventDefault();
