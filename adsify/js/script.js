d1638186889722fa636d01e0272d643f7a9a0692
ferzb <vorynkavetal@gmail.com>
2019-01-08 20:16:09 +0200

    sdf
    asdf

--- adsify/js/script.js
+++ adsify/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	var toggleBlock = $('.nav-toggle-block'),
 		toggleLine = $('.nav-toggle-line'),
+		menuList = $('.menu-list').find('> li'),
+		links = menuList.find('> a'),
 		nav = $('#nav');
 
 	toggleBlock.on('click', function(event) {
@@ -10,10 +12,36 @@ $(document).ready(function() {
 	});
 
 	$(window).resize(function() {
-		if ($(window).width() > 780) {
+		if ($(window).width() > 960) {
 			nav.removeClass('active');
 			toggleLine.removeClass('active-line');
 			toggleBlock.removeClass('open');
+		}
+	});
+
+
+	links.on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    	nav.removeClass('active');
+    	toggleLine.removeClass('active-line');
+    });
+
+    $('body').append('<i class="btn-up">');
+
+	$('.btn-up').click(function(){
+		$('body').animate({'scrollTop': 0}, 1000);
+		$('html').animate({'scrollTop': 0}, 1000)
+	})
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > 200){
+			$('.btn-up').addClass('active');
+		}
+		else{
+			$('.btn-up').removeClass('active');
 		}
 	});
 });
