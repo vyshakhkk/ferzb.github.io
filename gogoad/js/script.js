befcef814bc5b1285de8613b01fb8b82bcc23e1b
ferzb <vorynkavetal@gmail.com>
2019-01-24 21:54:44 +0200

    sdf
    sdf

--- gogoad/js/script.js
+++ gogoad/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	var categoryTitleList = $('.title-list'),
+		categoryTitleListSpan = $('.title-list').find('span'),
 		categoryBlock = $('.category-block'),
-		toggleCategoryBlock = $('.toggle-category-block');
+		toggleCategoryBlock = $('.toggle-category-block'),
+		toggleTopSearch = $('.toggle-top-search');
 
 	categoryBlock.each(function() {
 		var this_categoryBlock = $(this);
@@ -10,12 +12,13 @@ $(document).ready(function() {
 			$(this).toggleClass('active');
 		});
 	});
-	categoryTitleList.each(function() {
-		var this_categoryTitleList = $(this);
-
-		this_categoryTitleList.click(function() {
-			$(this).toggleClass('active');
-			var this_titleListUl = this_categoryTitleList.find('ul')
+	
+	categoryTitleListSpan.each(function() {
+		var this_categoryTitleListSpan = $(this)
+		this_categoryTitleListSpan.click(function() {
+			var this_categoryTitleList = this_categoryTitleListSpan.parents('li');
+			this_categoryTitleList.toggleClass('active');
+			var this_titleListUl = this_categoryTitleListSpan.siblings('ul');
 			this_titleListUl.slideToggle();
 		});
 	});
@@ -31,6 +34,31 @@ $(document).ready(function() {
 		});
 	});
 
+	$(function() {
+		$(window).scroll(function(){
+			if  ($(window).scrollTop() > 180)
+				toggleTopSearch.addClass('visibility');
+			else
+				toggleTopSearch.removeClass('visibility');
+		});
+	});
+
+	$('.popular-block-carousel').owlCarousel({
+		items:4,
+		lazyLoad:true,
+		nav:true,
+		dots:false,
+		margin: 20
+	});
+
+	$('.item-foto-carousel').owlCarousel({
+		items:1,
+		lazyLoad:true,
+		loop:true,
+		nav:true,
+		dots:false,
+		margin: 10
+	});
 	
 	$(function(){
 		var input = $('.input-global')
