e63368f85d3ab6538ee474d58c058adee6d6b41b
ferzb <vorynkavetal@gmail.com>
2019-03-14 19:31:34 +0200

    fh
    hg

--- opus/js/script.js
+++ opus/js/script.js
@@ -54,9 +54,11 @@ $(document).ready(function() {
 	$(window).scroll(function(){
 		if ($(window).scrollTop() > 200){
 			$('.btn-up').addClass('active');
+			$('header').addClass('fixed')
 		}
 		else{
 			$('.btn-up').removeClass('active');
+			$('header').removeClass('fixed')
 		}
 	});
 	
